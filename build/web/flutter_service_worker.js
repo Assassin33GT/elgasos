2b7d62b07c586a96d6a71b26e801752b5f7cc5de
Assassin33GT <bodeidris33@gmail.com>
2025-09-04 04:39:49 +0300

    web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -60,7 +60,7 @@ const RESOURCES = {".dart_tool/dartpad/web_plugin_registrant.dart": "7ed35bc85b7
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "95f8615e495d85f9477f19f01652b6b1",
+"flutter_bootstrap.js": "445f18d24c37d7365511aa15f69b775a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
